e88c0f53e0590a95b0e090e7870f869abdae9047
Amogh Karve <amogh.karve@gmail.com>
2018-04-15 16:49:45 +0530

    Licenses Added

--- plagcheck/plagiarism_form.php
+++ plagcheck/plagiarism_form.php
@@ -1,5 +1,20 @@
 <?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 require_once($CFG->dirroot.'/lib/formslib.php');
 
 class plagiarism_setup_form extends moodleform {
@@ -20,4 +35,3 @@ class plagiarism_setup_form extends moodleform {
         $this->add_action_buttons(true);
     }
 }
-
